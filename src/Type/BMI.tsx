ea154c1b005d83cb3bdb18138bbbce750690119d
hoang2001lg <anhyeuem2001lg@gmail.com>
2022-11-16 00:32:15 +0700

    tinh BMI

---
+++ src/Type/BMI.tsx
@@ -0,0 +1,4 @@
+export type BMI ={
+    weight:number,
+    height:number
+}
--- src/pages/Princing-plan.tsx
+++ src/pages/Princing-plan.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
 import { PackagesType } from '../Type/Packages'
 
 type PrincingListProps = {
