ab65171987e76c935f14b15648f2b5b29ffc1364
hoang2001lg <anhyeuem2001lg@gmail.com>
2022-11-17 23:09:37 +0700

    totalcart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Route, Routes } from "react-router-dom"
-import { listpack } from "./api/princing"
+import toastr from 'toastr';
+import { listpack, readpack } from "./api/princing"
 import Blog from "./pages/Blog"
 import BlogDetail from "./pages/Blog-detail"
 import CartPage from "./pages/Cart-page"
@@ -14,10 +15,35 @@ import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import TrainerDetail from "./pages/Trainer-detail"
 import { PackagesType } from "./Type/Packages"
+import { addToCart, decreaseItemInCart, increaseItemInCart, removeItemInCart } from "./ulltis/cart"
 
 function App() {
   const [packagess, setPackagess] = useState<PackagesType[]>([])
+  const [cart, setCart] = useState<PackagesType[]>([]);
+// Cart
+const onHandleAddToCart = async (id: number) => {
+  const { data } = await readpack(id)
+  addToCart({ ...data, quantity: 1 }, function () {
+    toastr.success(`Thêm ${data.name} vào giỏ hàng thành công!`)
+    setCart(JSON.parse(localStorage.getItem('cart') as string))
+  })
+}
+const onHandleIncreaseItemInCart = (id: number) => {
+  increaseItemInCart(id, () => {
+    setCart(JSON.parse(localStorage.getItem('cart') as string))
+  })
+}
+const onHandleDecreaseItemInCart = (id: number) => {
+  decreaseItemInCart(id, () => {
+    setCart(JSON.parse(localStorage.getItem('cart') as string))
+  })
+}
 
+const onHandleRemoveCart = (id: number) => {
+  removeItemInCart(id, () => {
+    setCart(JSON.parse(localStorage.getItem('cart') as string))
+  })
+}
   useEffect(() => {
     const getPackagess = async () => {
       const { data } = await listpack();
@@ -36,8 +62,8 @@ function App() {
           <Route path="blog/blogdetail" element={<BlogDetail />} />
           <Route path="contact" element={<Contact />} />
           <Route path="packagess" element={<PrincingPlan packagess={packagess} />} />
-          <Route path="packagess/packdetail/:id" element={<PackageDetail />} />
-          <Route path="cart/:id" element={<CartPage />} />
+          <Route path="packagess/packdetail/:id" element={<PackageDetail packagess={packagess} onAddToCart={onHandleAddToCart} />} />
+          <Route path="cart/:id" element={<CartPage onRemoveCart={onHandleRemoveCart} onDecreaseItemInCart={onHandleDecreaseItemInCart} onIncreaseItemInCart={onHandleIncreaseItemInCart} />} />
           <Route path="signup" element={<Signup />} />
           <Route path="signin" element={<Signin />} />
         </Route>
